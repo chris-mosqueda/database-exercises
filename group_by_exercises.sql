3df6361e6169ce7f75ae552156f613ddfe5fa07d
Christopher Mosqueda <christophermosqueda@Christophers-MacBook-Pro.local>
2018-08-15 09:22:18 -0500

    Updated mysql-group-by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,9 +12,18 @@ select last_name, first_name from employees
 where last_name like 'E%' and last_name like '%E'
 group by last_name, first_name;
 
+-- #5
+select last_name, count(*)
+from employees
+where last_name like '%q%'
+and last_name not like '%qu%'
+group by last_name
+order by count(*);
+-- or this solution for total count
 select count(last_name) from employees
 where last_name like '%q%' and last_name not like '%qu%'
 order by last_name;
+
 
 select count(gender), gender from employees
 where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
